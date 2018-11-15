39411c4535db072f6d949d6afb670e90b9923b0a
GamerSim <44953626+GamerSim@users.noreply.github.com>
2018-11-16 00:06:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "510507583174606869"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "512747923482869760"; // ايدي السررفر
+var channel = "512749192230469644";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft Minecraft  **')
     },305);
